ebbc0780ab609b91e2104f8108d6c4aeffb98f07
Minyoung Lee <mminy62@gmail.com>
2025-01-20 09:44:17 +0900

    Chore: Search 폴더링

---
+++ PhotoSearch/PhotoSearch/Search/EmptyCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  EmptyCollectionViewCell.swift
+//  PhotoSearch
+//
+//  Created by 이민영 on 1/20/25.
+//
+
+import UIKit
+import SnapKit
+
+class EmptyCollectionViewCell: UICollectionViewCell {
+    static let id = "EmptyCollectionViewCell"
+    private let titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        titleLabel.textAlignment = .center
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureData(itemFlag: Bool) {
+        titleLabel.text = itemFlag ? "검색결과가 없어요." : "사진을 검색해보세요."
+    }
+    
+    
+}
